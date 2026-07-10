17f72d2640481781a567ff10f31497c58cbb9759
Francesco Buda <buda2.francesco@gmail.com>
2026-07-10 13:22:36 +0200

    feat: add HelloItemView registration and ribbon icon for activating the view

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
 	MyPluginSettings,
 	SampleSettingTab,
 } from './settings';
+import { HelloItemView, VIEW_TYPE_HELLO } from './obsidian/HelloItemView';
 
 export default class CaosmosPlugin extends Plugin {
 	settings!: MyPluginSettings;
@@ -11,9 +12,37 @@ export default class CaosmosPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 		this.addSettingTab(new SampleSettingTab(this.app, this));
+
+		this.registerView(
+			VIEW_TYPE_HELLO,
+			(leaf) => new HelloItemView(leaf)
+		);
+
+		this.addRibbonIcon('sparkles', 'Apri Hello Caosmos', () => {
+			this.activateView();
+		});
 	}
 
 	onunload() {}
+
+	async activateView() {
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_HELLO);
+
+		const leaf = this.app.workspace.getRightLeaf(false);
+		if (!leaf) {
+			return;
+		}
+
+		await leaf.setViewState({
+			type: VIEW_TYPE_HELLO,
+			active: true,
+		});
+
+		const leaves = this.app.workspace.getLeavesOfType(VIEW_TYPE_HELLO);
+		if (leaves.length > 0) {
+			this.app.workspace.revealLeaf(leaves[0]!);
+		}
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign(
